afa7a89fa7cd90a2889ffcbbf83b1ecd0788a7e7
Andy Fowler <andy@andyfowler.com>
2010-07-01 16:34:25 -0400

    fixes #24, captureScreenshotToString

--- PHPUnit/Extensions/SeleniumTestCase/Driver.php
+++ PHPUnit/Extensions/SeleniumTestCase/Driver.php
@@ -592,9 +592,7 @@ class PHPUnit_Extensions_SeleniumTestCase_Driver
             case 'attachFile':
             case 'break':
             case 'captureEntirePageScreenshot':
-            case 'captureEntirePageScreenshotToString':
             case 'captureScreenshot':
-            case 'captureScreenshotToString':
             case 'check':
             case 'chooseCancelOnNextConfirmation':
             case 'chooseOkOnNextConfirmation':
@@ -703,8 +701,7 @@ class PHPUnit_Extensions_SeleniumTestCase_Driver
                     case 'allowNativeXpath':
                     case 'assignId':
                     case 'captureEntirePageScreenshot':
-                    case 'captureScreenshot':
-                    case 'captureScreenshotToString': {
+                    case 'captureScreenshot': {
                         // intentionally empty
                     }
                     break;
@@ -783,6 +780,8 @@ class PHPUnit_Extensions_SeleniumTestCase_Driver
             case 'getTable':
             case 'getText':
             case 'getTitle':
+            case 'captureEntirePageScreenshotToString':
+            case 'captureScreenshotToString':
             case 'getValue': {
                 $result = $this->getString($command, $arguments);
 
