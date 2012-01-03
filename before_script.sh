45781dd6742bd3765d8fa420d10745088a755022
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-01-03 16:57:15 +0100

    Trying to synchronize with Selenium Server startup

--- before_script.sh
+++ before_script.sh
@@ -7,3 +7,4 @@ sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
 java -jar selenium-server-standalone-2.15.0.jar & > /dev/null
+sleep 2
