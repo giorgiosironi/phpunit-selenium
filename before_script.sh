2b9dfaa5876fa14bab68c214b3251a76fac1f2ae
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-01-17 17:15:52 +0100

    Redirecting verbose stdout and stderr from Selenium Server

--- before_script.sh
+++ before_script.sh
@@ -6,5 +6,5 @@ cd ..
 sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-java -jar selenium-server-standalone-2.15.0.jar & > /dev/null 2>&1
+java -jar selenium-server-standalone-2.15.0.jar > /dev/null 2>&1 &
 sleep 2
