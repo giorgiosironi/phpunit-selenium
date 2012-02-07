5d0e6831b3c8fece8a237025d81212fea8ed3b4c
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-02-07 18:28:06 +0100

    Trying to fix Travis CI build

--- before_script.sh
+++ before_script.sh
@@ -1,10 +1,10 @@
 git submodule init
 git submodule update
 cd selenium-1-tests
-python -m SimpleHTTPServer 8080 &
+python -m SimpleHTTPServer 8080 > /dev/null 2>&1 &
 cd ..
 sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.15.0.jar
-java -jar selenium-server-standalone-2.15.0.jar > /dev/null 2>&1 &
+java -jar selenium-server-standalone-2.15.0.jar #> /dev/null 2>&1 &
 sleep 2
