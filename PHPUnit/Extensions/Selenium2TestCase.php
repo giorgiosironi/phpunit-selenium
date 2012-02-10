f977270f473d52465f652037e703c1de2228bd81
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-02-10 16:04:11 +0100

    Added sensible defaults for host and port of the Selenium Server

--- PHPUnit/Extensions/Selenium2TestCase.php
+++ PHPUnit/Extensions/Selenium2TestCase.php
@@ -74,12 +74,12 @@ abstract class PHPUnit_Extensions_Selenium2TestCase extends PHPUnit_Framework_Te
     /**
      * @var string
      */
-    private $host;
+    private $host = 'localhost';
 
     /**
      * @var int
      */
-    private $port;
+    private $port = 4444;
 
     /**
      * @var string
