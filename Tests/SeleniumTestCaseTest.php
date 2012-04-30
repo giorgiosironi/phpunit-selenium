050bba5059830e8369fa9e1f8e53c00079a872e5
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-04-30 12:03:42 +0200

    List The BSD 3-Clause License correctly and consistently. Thanks to Christian Aistleitner <christian@quelltextlich.at> for bringing this issue to my attention.

--- Tests/SeleniumTestCaseTest.php
+++ Tests/SeleniumTestCaseTest.php
@@ -39,7 +39,7 @@
  * @author     Shin Ohno <ganchiku@gmail.com>
  * @author     Giorgio Sironi <giorgio.sironi@asp-poli.it>
  * @copyright  2010-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://www.phpunit.de/
  * @since      File available since Release 1.0.0
  */
@@ -51,7 +51,7 @@
  * @author     Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @author     Shin Ohno <ganchiku@gmail.com>
  * @copyright  2010-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @version    Release: @package_version@
  * @link       http://www.phpunit.de/
  * @since      Class available since Release 1.0.0
