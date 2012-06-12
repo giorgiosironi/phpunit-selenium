7296a931e509891045d53f603acab9eb9fcde10b
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-06-12 11:27:03 +0200

    Moved append, prepend and coverage script into SeleniumCommon as they will be used by both Selenium 1 and 2 test cases

--- Tests/Selenium2TestCase/Coverage/singleFile.php
+++ Tests/Selenium2TestCase/Coverage/singleFile.php
@@ -1,9 +1,9 @@
 <?php
 $_COOKIE['PHPUNIT_SELENIUM_TEST_ID'] = 'dummyTestId';
-require __DIR__ . '/../../../PHPUnit/Extensions/SeleniumTestCase/prepend.php';
+require __DIR__ . '/../../../PHPUnit/Extensions/SeleniumCommon/prepend.php';
 
 require_once 'DummyClass.php';
 $object = new DummyClass();
 $object->coveredMethod();
 
-require __DIR__ . '/../../../PHPUnit/Extensions/SeleniumTestCase/append.php';
+require __DIR__ . '/../../../PHPUnit/Extensions/SeleniumCommon/append.php';
--- Tests/Selenium2TestCase/Coverage/singleFileCoverage.php
+++ Tests/Selenium2TestCase/Coverage/singleFileCoverage.php
@@ -1,3 +1,3 @@
 <?php
 $_GET['PHPUNIT_SELENIUM_TEST_ID'] = $argv[1];
-require __DIR__ . '/../../../PHPUnit/Extensions/SeleniumTestCase/phpunit_coverage.php';
+require __DIR__ . '/../../../PHPUnit/Extensions/SeleniumCommon/phpunit_coverage.php';
