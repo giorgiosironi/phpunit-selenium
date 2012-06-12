c5adeacddb6ec1479533ffdacbda312c3d9248b0
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-06-12 11:20:36 +0200

    Extended tests of coverage on phpunit_coverage.php script

--- Tests/Selenium2TestCase/Coverage/SingleFileTest.php
+++ Tests/Selenium2TestCase/Coverage/SingleFileTest.php
@@ -6,22 +6,41 @@ class Tests_Selenium2TestCase_Coverage_SingleFileTest extends PHPUnit_Framework_
     public function setUp()
     {
         $this->coverageFilePattern = __DIR__ . '/*.' . $this->dummyTestId;
-        $coverageFiles = glob($this->coverageFilePattern);
-        foreach ($coverageFiles as $file) {
-            unlink($file);
-        }
+        $this->dummyClassSourceFile = __DIR__ . '/DummyClass.php';
     }
 
     public function testExecutingAFileWithThePrependedAndAppendedCoverageScriptsProducesACoverageData()
     { 
-        $output = exec('php ' . __DIR__ . '/singleFile.php');
+        $this->clearCoverageFiles();
+
+        exec('php ' . __DIR__ . '/singleFile.php');
         $coverageFiles = glob($this->coverageFilePattern);
         $this->assertEquals(1, count($coverageFiles));
 
         $content = unserialize(file_get_contents($coverageFiles[0]));
-        $dummyClassCoverage = $content[__DIR__ . '/DummyClass.php'];
+        $dummyClassCoverage = $content[$this->dummyClassSourceFile];
         $this->assertCovered(6, $dummyClassCoverage);
         $this->assertNotCovered(11, $dummyClassCoverage);
+
+        return $dummyClassCoverage;
+    }
+
+    /**
+     * @depends testExecutingAFileWithThePrependedAndAppendedCoverageScriptsProducesACoverageData
+     */
+    public function testTheCoverageScriptReturnsTheContentOfASpecificCoverageFile($expectedDummyClassCoverage)
+    {
+        $coverage = unserialize(exec('php ' . __DIR__ . '/singleFileCoverage.php ' . $this->dummyTestId));
+        $dummyClassCoverage = $coverage[$this->dummyClassSourceFile];
+        $this->assertEquals($expectedDummyClassCoverage, $dummyClassCoverage['coverage']);
+    }
+
+    private function clearCoverageFiles()
+    {
+        $coverageFiles = glob($this->coverageFilePattern);
+        foreach ($coverageFiles as $file) {
+            unlink($file);
+        }
     }
 
     private function assertCovered($line, array $fileCoverage)
