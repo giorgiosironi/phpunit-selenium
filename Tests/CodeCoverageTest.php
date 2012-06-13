3d6b9bd1d5ead2e8bbedcad6c4f63c52becb8ef4
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-06-13 09:59:18 +0200

    Skipping test before setUp() takes place

--- Tests/CodeCoverageTest.php
+++ Tests/CodeCoverageTest.php
@@ -5,13 +5,13 @@ class CodeCoverageTest extends PHPUnit_Extensions_Selenium2TestCase
 
     public function setUp()
     {
+        $this->markTestIncomplete('Would require PHP 5.4 for running .php files on the server');
         $this->setBrowser(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM2_BROWSER);
         $this->setBrowserUrl('http://localhost/');
     }
 
     public function testCoverageIsRetrieved()
     {
-        $this->markTestIncomplete('Would require PHP 5.4 for running .php files on the server');
         $this->url('example.php');
     }
 }
