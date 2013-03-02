5e0401266a125c0afdf6841f7422d043862a4195
Giorgio Sironi <info@giorgiosironi.com>
2013-03-02 19:11:33 +0100

    Debugging Travis CI

--- before_script.sh
+++ before_script.sh
@@ -11,6 +11,6 @@ sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 echo "Starting Selenium"
 wget http://selenium.googlecode.com/files/selenium-server-standalone-2.30.0.jar
-java -jar selenium-server-standalone-2.30.0 > /dev/null 2>&1 &
+java -jar selenium-server-standalone-2.30.0.jar > /dev/null 2>&1 &
 sleep 30
 echo "Finished setup"
