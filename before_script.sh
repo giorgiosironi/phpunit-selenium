c8b9495ca43aa6317124a45b7684cfbccbda947b
Giorgio Sironi <info@giorgiosironi.com>
2013-04-25 11:49:19 +0200

    Updating Selenium Server to 2.32.0

--- before_script.sh
+++ before_script.sh
@@ -10,7 +10,7 @@ echo "Starting xvfb"
 sh -e /etc/init.d/xvfb start
 export DISPLAY=:99.0
 echo "Starting Selenium"
-wget http://selenium.googlecode.com/files/selenium-server-standalone-2.30.0.jar
-java -jar selenium-server-standalone-2.30.0.jar > /dev/null 2>&1 &
+wget http://selenium.googlecode.com/files/selenium-server-standalone-2.32.0.jar
+java -jar selenium-server-standalone-2.32.0.jar > /dev/null 2>&1 &
 sleep 30
 echo "Finished setup"
