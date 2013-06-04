852294eea57234e34c3e5f9b1c5179b2cf8868a4
chr0n1x <heilong24@gmail.com>
2013-06-03 22:11:45 -0400

    SessionCommand_FileTest

---
+++ Tests/Selenium2TestCase/SessionCommand/FileTest.php
@@ -0,0 +1,37 @@
+<?php
+
+class Tests_Selenium2TestCase_SessionCommand_FileTest extends Tests_Selenium2TestCase_BaseTestCase
+{
+
+  /**
+   * @test
+   */
+  public function uploadFile() {
+
+    $this->url( 'php/file_upload.php' );
+
+    $remote_file      = $this->file( 'html/banner.gif' );
+
+    $upload_criteria  = $this->using( 'name' )
+                             ->value( 'upload_here' );
+
+    $submit_criteria  = $this->using( 'name' )
+                             ->value( 'submit' );
+
+    $msg_criteria     = $this->using( 'id' )
+                             ->value( 'uploaded' );
+
+    $this->element( $upload_criteria )
+         ->value( array( 'value' => $remote_file ) );
+
+    $this->element( $submit_criteria )
+         ->click();
+
+    $msg_displayed    = $this->element( $msg_criteria )
+                             ->displayed();
+
+    $this->assertNotEmpty( $msg_displayed );
+
+  } // uploadFile
+
+} // Tests_Selenium2TestCase_SessionCommand_FileTest
