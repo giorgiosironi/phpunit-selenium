02f84ab722dcb7c3fd2e51d93237e48d4cbc2dca
Giorgio Sironi <info@giorgiosironi.com>
2013-06-15 11:03:35 +0200

    Trying to use a fixed version of Firefox

--- before_script.sh
+++ before_script.sh
@@ -1,6 +1,8 @@
 serverUrl='http://127.0.0.1:4444'
 serverVersion='2.32.0'
 serverFile=selenium-server-standalone-$serverVersion.jar
+firefoxUrl=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/21.0/linux-x86_64/en-US/firefox-21.0.tar.bz2
+firefoxFile=firefox.tar.bz2
 fixturePort=8080
 phpVersion=`php -v`
 
@@ -28,12 +30,16 @@ else
 fi
 cd ..
 
+echo "Downloading Firefox"
+wget $firefoxUrl -O $firefoxFile
+tar xvjf $firefoxFile
+
 echo "Starting xvfb"
 echo "Starting Selenium"
 if [ ! -f $serverFile ]; then
     wget http://selenium.googlecode.com/files/selenium-server-standalone-$serverVersion.jar -O $serverFile
 fi
-xvfb-run java -jar $serverFile > /dev/null 2>&1 &
+xvfb-run java -Dwebdriver.firefox.bin=firefox/firefox-bin  -jar $serverFile > /dev/null 2>&1 &
 
 wget --retry-connrefused --tries=60 --waitretry=1 --output-file=/dev/null $serverUrl/wd/hub/status -O /dev/null
 if [ ! $? -eq 0 ]; then
