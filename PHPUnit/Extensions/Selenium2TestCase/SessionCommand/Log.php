abf9d2c6aed124144c4e921e1ae5d524f99de349
Dharrya <dharrya@gmail.com>
2013-08-25 13:08:16 +0300

    Changed @since tag from 1.2.0 to 1.3.2

--- PHPUnit/Extensions/Selenium2TestCase/SessionCommand/Log.php
+++ PHPUnit/Extensions/Selenium2TestCase/SessionCommand/Log.php
@@ -39,7 +39,7 @@
  * @copyright  2010-2013 Sebastian Bergmann <sebastian@phpunit.de>
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @link       http://www.phpunit.de/
- * @since      File available since Release 1.2.0
+ * @since      File available since Release 1.3.2
  */
 
 /**
@@ -51,7 +51,7 @@
  * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @version    Release: @package_version@
  * @link       http://www.phpunit.de/
- * @since      Class available since Release 1.2.0
+ * @since      Class available since Release 1.3.2
  */
 class PHPUnit_Extensions_Selenium2TestCase_SessionCommand_Log
     extends PHPUnit_Extensions_Selenium2TestCase_Command
