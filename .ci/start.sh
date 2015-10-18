f67f1d37402ab4412725431d3b508e01d873abe4
Giorgio Sironi <info@giorgiosironi.com>
2015-10-18 14:37:13 +0200

    Aggressive reset of daemons

--- .ci/start.sh
+++ .ci/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-killall -9 java
-killall -9 Xvfb
-sudo supervisorctl reload
+sudo killall supervisord
+sudo killall -9 java
+sudo killall -9 Xvfb
+sudo rm -f /tmp/.X99-lock
+sudo /etc/init.d/supervisor start
 
 wget --retry-connrefused --tries=120 --waitretry=3 --output-file=/dev/null "$SELENIUM_HUB_URL/wd/hub/status" -O /dev/null
 if [ ! $? -eq 0 ]; then
