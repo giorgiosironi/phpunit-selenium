d8e8bd9ea90a7248ecec97c533164f41d5731798
Giorgio Sironi <info@giorgiosironi.com>
2015-10-18 14:01:41 +0200

    Upgraded Selenium Server to last stable version

--- .ci/common_env.sh
+++ .ci/common_env.sh
@@ -2,5 +2,5 @@
 
 SELENIUM_HUB_URL='http://127.0.0.1:4444'
 SELENIUM_JAR=/usr/share/selenium/selenium-server-standalone.jar
-SELENIUM_DOWNLOAD_URL=http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar
+SELENIUM_DOWNLOAD_URL=http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar
 PHP_VERSION=$(php -v)
