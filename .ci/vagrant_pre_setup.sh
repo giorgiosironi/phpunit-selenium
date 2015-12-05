1d0f044fb8619cdf27f7245ea862579debe6f77a
Giorgio Sironi <info@giorgiosironi.com>
2015-12-05 11:18:47 +0100

    Upgrading PHP of VM to 5.6

--- .ci/vagrant_pre_setup.sh
+++ .ci/vagrant_pre_setup.sh
@@ -6,6 +6,11 @@ sed -i "1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restr
 sed -i "1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse" /etc/apt/sources.list
 sed -i "1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse" /etc/apt/sources.list
 
+apt-get install python-software-properties
+
+apt-add-repository ppa:ondrej/php5-5.6 -y
+apt-get update
+
 # installing xvfb, java and php
 apt-get install xvfb openjdk-7-jre-headless php5-cli php5-curl php5-xdebug ncurses-term unzip xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic -y --no-install-recommends
 
