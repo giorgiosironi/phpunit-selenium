42d4b86054dbde5c14e22b711753bda1583bb917
paulbriton <paull.briton@gmail.com>
2016-11-25 16:56:06 +0100

    Upgrade selenium version to 3.0.1, add support for Firefox 50 and geckodriver.

--- .ci/vagrant_pre_setup.sh
+++ .ci/vagrant_pre_setup.sh
@@ -6,11 +6,16 @@ sed -i "1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restr
 sed -i "1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse" /etc/apt/sources.list
 sed -i "1ideb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse" /etc/apt/sources.list
 
-apt-get install python-software-properties
+apt-get update
+
+apt-get install software-properties-common -y
+apt-get install python-software-properties -y
 
 apt-add-repository ppa:ondrej/php5-5.6 -y
+apt-add-repository ppa:openjdk-r/ppa -y
+
 apt-get update
 
 # installing xvfb, java and php
-apt-get install xvfb openjdk-7-jre-headless php5-cli php5-curl php5-xdebug ncurses-term unzip xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic vim -y --no-install-recommends
+apt-get install xvfb openjdk-8-jre-headless php5-cli php5-curl php5-xdebug ncurses-term unzip xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic vim -y --no-install-recommends
 
