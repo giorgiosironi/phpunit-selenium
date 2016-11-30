5dcb6ca93bf41369b91904b9c99c340a02161cf8
paulbriton <paull.briton@gmail.com>
2016-11-30 14:34:27 +0100

    Repair broken vagrant setup after last commit

--- .ci/vagrant_pre_setup.sh
+++ .ci/vagrant_pre_setup.sh
@@ -13,6 +13,8 @@ apt-get install python-software-properties -y
 
 apt-add-repository ppa:ondrej/php -y
 
+apt-get update
+
 # installing xvfb, java and php
 apt-get install xvfb php5.6-cli php5.6-curl php5.6-xml php-xdebug ncurses-term unzip xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic vim -y --no-install-recommends
 
