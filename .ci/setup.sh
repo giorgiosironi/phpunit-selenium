9da8144a7aca72653a6ff2b00e5179abaa55a8c0
Patrik Štrba <patrik.strba@gmail.com>
2019-12-18 16:41:41 +0100

    Fix checking composer in travis

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "/usr/local/bin/composer" ]; then
+if [ ! -x "$(command -v composer)" ]; then
     echo "Installing Composer"
     php -r "readfile('https://getcomposer.org/installer');" | sudo php -d apc.enable_cli=0 -- --install-dir=/usr/local/bin --filename=composer
 else
     echo "Updating Composer"
-    sudo /usr/local/bin/composer self-update
+    sudo composer self-update
 fi
 
 echo "Installing dependencies"
