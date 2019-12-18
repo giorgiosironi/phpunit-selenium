1a78dba05b89b1e41d979194d6cefc78aaf9315e
Patrik Štrba <patrik.strba@gmail.com>
2019-12-18 15:36:16 +0100

    Optimize order installing packages

--- .ci/vagrant_pre_setup.sh
+++ .ci/vagrant_pre_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-apt-get install software-properties-common -y
-apt-get install python-software-properties -y
-
 apt-add-repository ppa:ondrej/php -y
 
 apt-get update
+
+apt-get install software-properties-common -y
+apt-get install python-software-properties -y
 
 # installing xvfb, java and php
 apt-get install xvfb php7.1-cli php7.1-curl php7.1-xml php7.1-mbstring php-xdebug ncurses-term unzip xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic vim -y --no-install-recommends
