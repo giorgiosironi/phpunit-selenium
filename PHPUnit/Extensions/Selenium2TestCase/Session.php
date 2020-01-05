9c17674b571c72448bf044d2dbf21175530e4408
Patrik Štrba <patrik.strba@gmail.com>
2020-01-05 18:10:46 +0100

    Change PSR0 PHPUnit_Extensions_Selenium2TestCase_Session_SessionCommand_AcceptAlert to PSR4 \PHPUnit\Extensions\Selenium2TestCase\SessionCommand\AcceptAlert

--- PHPUnit/Extensions/Selenium2TestCase/Session.php
+++ PHPUnit/Extensions/Selenium2TestCase/Session.php
@@ -52,6 +52,7 @@ use PHPUnit\Extensions\Selenium2TestCase\ElementCommand\GenericPost;
 use PHPUnit\Extensions\Selenium2TestCase\Session\Cookie;
 use PHPUnit\Extensions\Selenium2TestCase\Session\Storage;
 use PHPUnit\Extensions\Selenium2TestCase\Session\Timeouts;
+use PHPUnit\Extensions\Selenium2TestCase\SessionCommand\AcceptAlert;
 use PHPUnit_Extensions_Selenium2TestCase_SessionCommand_Active;
 use PHPUnit_Extensions_Selenium2TestCase_SessionCommand_GenericAttribute;
 use PHPUnit_Extensions_Selenium2TestCase_SessionCommand_Url;
@@ -128,7 +129,7 @@ class Session extends Accessor
     {
         $baseUrl = $this->baseUrl;
         return array(
-            'acceptAlert' => 'PHPUnit_Extensions_Selenium2TestCase_SessionCommand_AcceptAlert',
+            'acceptAlert' => AcceptAlert::class,
             'alertText' => 'PHPUnit_Extensions_Selenium2TestCase_SessionCommand_AlertText',
             'back' => GenericPost::class,
             'click' => 'PHPUnit_Extensions_Selenium2TestCase_SessionCommand_Click',
