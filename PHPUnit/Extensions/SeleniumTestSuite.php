8fe80b19b17f01779b8a36f8aed0e9989c1551e0
Patrik Štrba <patrik.strba@gmail.com>
2020-10-02 14:01:08 +0200

    Fix method filter

--- PHPUnit/Extensions/SeleniumTestSuite.php
+++ PHPUnit/Extensions/SeleniumTestSuite.php
@@ -157,6 +157,10 @@ class SeleniumTestSuite extends TestSuite
         else {
             // Create tests from test methods for single browser.
             foreach ($class->getMethods() as $method) {
+                if (!TestUtil::isTestMethod($method)) {
+                    continue;
+                }
+
                 $suite->addTestMethod($class, $method);
             }
         }
